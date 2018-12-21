dff4896600649756d45e69b24497b10fa73f3215
Morgan Creekmore <morgan@creekmore.email>
2018-12-20 23:59:53 -0600

    Change all properties to be of type Layout to resolve #7

--- src/NLog.Targets.Redis.Tests/Mocks/MockRedisTarget.cs
+++ src/NLog.Targets.Redis.Tests/Mocks/MockRedisTarget.cs
@@ -15,9 +15,9 @@ namespace NLog.Targets.Redis.Tests.Mocks
             _connectionMultiplexer = connectionMultiplexer;
         }
 
-        internal override RedisConnectionManager CreateConnectionManager()
+        internal override RedisConnectionManager CreateConnectionManager(string host, int port, int db, string password)
         {
-            return new MockRedisConnectionManager(Host, Port, Db, Password, _connectionMultiplexer);
+            return new MockRedisConnectionManager(host, port, db, password, _connectionMultiplexer);
         }
     }
 }
